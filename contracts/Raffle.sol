58dedeaf62d6df099d6ba42f7353cae29ccc1947
Nawodya Ishan <nawodyain@gmail.com>
2024-01-05 08:07:34 +0530

    feat: add contact and resolve dep errors

---
+++ contracts/Raffle.sol
@@ -0,0 +1,13 @@
+// Raffle
+// Enter the lottery
+// Pick a Random Winner
+// Winner Should selected automatically { Every x minutes }
+// ChainLink Oracle => Randomness and Automated Execution (Off Chain ChainLink Keepers)
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract Raffle {
+
+}
