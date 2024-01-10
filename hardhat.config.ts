cac863719a5c5b8c062742759457d1429693776a
Nawodya Ishan <nawodyain@gmail.com>
2024-01-10 14:35:22 +0530

    feat: add env variables

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,12 +17,14 @@ const MAINNET_RPC_URL =
     process.env.ALCHEMY_MAINNET_RPC_URL ||
     "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const SEPOLIA_RPC_URL =
-    process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY"
-const POLYGON_MAINNET_RPC_URL =
-    process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
+    process.env.SEPOLIA_RPC_URL || "https://sepolia.infura.io/v3/126fa49371d24f01b0ea6599f3307e63"
+const AVALANCHE_FUJI_RPC_URL =
+    process.env.AVALANCHE_FUJI_RPC_URL || "https://avalanche-fuji.infura.io/v3/126fa49371d24f01b0ea6599f3307e63"
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 // optional
 const MNEMONIC = process.env.MNEMONIC || "your mnemonic"
+
+const FUGI_RPC_URL = process.env.SEPOLIA_RPC_URL
 
 // Your API key for Etherscan, obtain one at https://etherscan.io/
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
@@ -33,14 +35,10 @@ const config: HardhatUserConfig = {
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
-            // // If you want to do some forking, uncomment this
-            // forking: {
-            //   url: MAINNET_RPC_URL
-            // }
             chainId: 31337,
         },
         localhost: {
-            chainId: 31337,
+            chainId: 31337
         },
         sepolia: {
             url: SEPOLIA_RPC_URL,
@@ -49,7 +47,7 @@ const config: HardhatUserConfig = {
             //     mnemonic: MNEMONIC,
             //   },
             saveDeployments: true,
-            chainId: 11155111,
+            chainId: 11155111
         },
         mainnet: {
             url: MAINNET_RPC_URL,
@@ -58,51 +56,51 @@ const config: HardhatUserConfig = {
             //     mnemonic: MNEMONIC,
             //   },
             saveDeployments: true,
-            chainId: 1,
+            chainId: 1
         },
-        polygon: {
-            url: POLYGON_MAINNET_RPC_URL,
+        fuji: {
+            url: AVALANCHE_FUJI_RPC_URL,
             accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
             saveDeployments: true,
-            chainId: 137,
-        },
+            chainId: 43113
+        }
     },
     etherscan: {
         // npx hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
         apiKey: {
             sepolia: ETHERSCAN_API_KEY,
-            polygon: POLYGONSCAN_API_KEY,
-        },
+            polygon: POLYGONSCAN_API_KEY
+        }
     },
     gasReporter: {
         enabled: false,
         currency: "USD",
         outputFile: "gas-report.txt",
-        noColors: true,
+        noColors: true
         // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
     },
     namedAccounts: {
         deployer: {
             default: 0, // here this will by default take the first account as deployer
-            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+            1: 0 // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
         },
         player: {
-            default: 1,
-        },
+            default: 1
+        }
     },
     solidity: {
         compilers: [
             {
-                version: "0.8.22",
+                version: "0.8.22"
             },
             {
-                version: "0.4.24",
-            },
-        ],
+                version: "0.4.24"
+            }
+        ]
     },
     mocha: {
-        timeout: 200000, // 200 seconds max for running tests
-    },
+        timeout: 200000 // 200 seconds max for running tests
+    }
 }
 
 export default config
